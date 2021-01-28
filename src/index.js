95886787ef3948eb731ba2f5f3085004dca3d6c5
Alejo <alejogarb25@gmail.com>
2021-01-28 11:25:00 -0300

    actualizar, arreglado incializar, y creado mostrarListadoMonedas

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ function configurarFecha () {
     //1. HOY crea nuevo objeto "Date", max necesita el formato YYYY-MM-DD
     //2. toISOString me lo soluciona
     //3. Split en la t, da array con dos posiciones. El [0] es la izquierda
-    
+
     $inputFecha.setAttribute= ("max", hoy);
-    $inputFecha.addEventListener("change", actualizar)
+    console.log($inputFecha.setAttribute)
+   // $inputFecha.addEventListener("change", actualizar)
 }
 
 function obtenerCambios (base = "EUR", fecha = "latest"){ // Parametros por default 
@@ -19,14 +20,38 @@ function obtenerCambios (base = "EUR", fecha = "latest"){ // Parametros por defa
     .then(r => r.rates) // sacamos el objetos de rates para despues sacarle la key
 }
 
+function mostrarListadoMonedas (base) {
+    container = document.createElement("div");
+    container.classlist.add("list-group");
+    monedas.sort().forEach(($item) => {
+        let $item = document.createElement("a")
+        $item.classlist.add("list-group-item");
+        $item.textContent = base;
+        $item.dataset.base = base;
+        $item.addEventListener("click", () => {
+            if ($item.class === "list-group-item-active") {
+                return undefined
+            }
+            $item.classlist.add("-active")
+            actualizar
+        })
+        $item.appendChild(monedas)
+    })
 
-funtcion obtenerMonedas () {
+}
+
+function obtenerMonedas () {
     return obtenerCambios().then(resultado => Object.keys(resultado).concat("EUR")) // Promesa. Una vez que obtengo cambios, saco las keys y agrego EUR
 }
 
-function inicializar (){
-    fetch()
-    .then()
-    .then()
-    mostrarListadoMonedas()
+function actualizar () {
+    mostrarCargando()
+    obtenerCambios(obtenerFechaSeleccionada(), obtenerBaseSeleccionada())
 }
+
+function inicializar (){
+    obtenerMonedas().then(mostrarListadoMonedas())
+    configurarFecha()
+}
+
+inicializar()
