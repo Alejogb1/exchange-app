1262b9400f1a62679d2208ce9b699097d3b9bda4
Alejo <alejogarb25@gmail.com>
2021-02-16 15:17:43 -0300

    modulos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-
+import {mostrarListadoMonedas, mostrarCambios} from "./ui.js"
+import {obtenerMonedas, obtenerCambios} from "./api.js"
+import {obtenerBaseSeleccionada, obtenerFechaSeleccionada} from "./cambios.js"
 function configurarFecha () {
     const $inputFecha = document.querySelector("#fecha-input")
     // formato YYYY-MM-DD
@@ -10,58 +12,17 @@ function configurarFecha () {
 
     $inputFecha.setAttribute= ("max", hoy);
     console.log($inputFecha.setAttribute)
-   // $inputFecha.addEventListener("change", actualizar)
+    $inputFecha.addEventListener("change", actualizar)
 }
 
-function obtenerCambios (base = "EUR", fecha = "latest"){ // Parametros por default 
-    const BASE_URL = "https://api.exchangeratesapi.io"
-    return fetch(`${BASE_URL}/${fecha}?base=${base}`)
-    .then(r => r.json())
-    .then(r => r.rates) // sacamos el objetos de rates para despues sacarle la key
+async function actualizar () {
+  //  mostrarCargando()
+    mostrarCambios(await obtenerCambios(obtenerBaseSeleccionada(), obtenerFechaSeleccionada()))
 }
 
-function mostrarListadoMonedas (cambios) {
-    let $container = document.createElement("div");
-    $container.classList.add("list-group");
-    cambios.sort().forEach((moneda) => {
-        let $item = document.createElement("a")
-        $item.classList.add("list-group-item", "list-group-item-action");
-        $item.textContent = moneda;
-        $item.dataset.base = moneda;
-        $item.addEventListener("click", () => {
-            const $itemActivo = document.querySelector(".list-group-item.active")
-            if ($itemActivo) {
-                console.log("borramos: ", $item)
-                return $itemActivo.classList.remove("active")
-            }
-            $item.classList.add("active")
-            //actualizar()
-        })
-        $container.appendChild($item)
-    })
-    document.querySelector("#monedas").appendChild($container)
-}
-
-function obtenerFechaSeleccionada () {
-    const fechaSeleccionada = document.querySelector("#fecha").value;
-    return fechaSeleccionada || undefined;
-}
-function obtenerBaseSeleccionada () {
-
-}
-
-function obtenerMonedas () {
-    return obtenerCambios().then(resultado => Object.keys(resultado).concat("EUR")) // Promesa. Una vez que obtengo cambios, saco las keys y agrego EUR
-}
-
-function actualizar () {
-    mostrarCargando()
-    obtenerCambios(obtenerBaseSeleccionada(), obtenerFechaSeleccionada())
-}
-
-function inicializar (){
-    obtenerMonedas().then(resultado => mostrarListadoMonedas(resultado))
-    configurarFecha()
+async function inicializar (){
+    mostrarListadoMonedas(await obtenerMonedas(), actualizar)
+    configurarFecha(actualizar)
 }
 
 inicializar()
